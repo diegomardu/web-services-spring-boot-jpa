551a7b8053658b31d5cf8a631039d6dcd1531226
Diego Martins <diegomartins836@gmail.com>
2021-08-29 00:09:46 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/dduarte/course/config/TestConfig.java
+++ src/main/java/com/dduarte/course/config/TestConfig.java
@@ -9,65 +9,77 @@ import org.springframework.context.annotation.Configuration;
 
 import com.dduarte.course.entities.Category;
 import com.dduarte.course.entities.Order;
+import com.dduarte.course.entities.OrderItem;
 import com.dduarte.course.entities.Product;
 import com.dduarte.course.entities.User;
 import com.dduarte.course.entities.enums.OrderStatus;
 import com.dduarte.course.repositories.CategoryRepository;
+import com.dduarte.course.repositories.OrderItemRepository;
 import com.dduarte.course.repositories.OrderRepository;
 import com.dduarte.course.repositories.ProductRepository;
 import com.dduarte.course.repositories.UserRepository;
 
 @Configuration
 public class TestConfig implements CommandLineRunner {
-	
+
 	@Autowired
 	private UserRepository userRepostory;
-	
+
 	@Autowired
 	private OrderRepository orderRespository;
-	
+
 	@Autowired
-	private CategoryRepository categoryRepository; 
-	
+	private CategoryRepository categoryRepository;
+
 	@Autowired
 	private ProductRepository productRespository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
-		
+
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-		
+
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRespository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		
+
 		productRespository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-		
+
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
+		userRepostory.saveAll(Arrays.asList(u1, u2));
+		orderRespository.saveAll(Arrays.asList(o1, o2, o3));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o1, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o1, p5, 2, p5.getPrice());
 		
-		userRepostory.saveAll(Arrays.asList(u1,u2));
-		orderRespository.saveAll(Arrays.asList(o1,o2,o3));
-		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
 	}
 
 }
--- src/main/java/com/dduarte/course/entities/Order.java
+++ src/main/java/com/dduarte/course/entities/Order.java
@@ -2,6 +2,8 @@ package com.dduarte.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.dduarte.course.entities.enums.OrderStatus;
@@ -36,6 +39,9 @@ public class Order implements Serializable {
 	@ManyToOne // Associação com a tabela de users
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")	
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Order() {
 
@@ -81,6 +87,10 @@ public class Order implements Serializable {
 		if(orderStatus != null) {
 			this.orderStatus = orderStatus.getCode();
 		}
+	}
+	
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
